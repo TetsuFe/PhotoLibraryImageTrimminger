70cddc7feef94b548aa8852e42f0b41fe12ff1f0
TetsuFe <tetsubeg1ner27@gmail.com>
2017-10-09 10:43:47 +0900

    newgameのサンプル画像を削除・それにともなうコード変更

--- PhotoLibraryImageTrimminger/Sources/AppDelegate.swift
+++ PhotoLibraryImageTrimminger/Sources/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var photoLibraryImage : UIImage! = UIImage(named: "newgame")
+    var photoLibraryImage : UIImage!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
